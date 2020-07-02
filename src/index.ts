e4b4d876f4cb458d0fea53bfb368f8655bc85364
vipcxj <vipcxj@126.com>
2020-07-03 02:19:25 +0800

    ready to release.

--- src/index.ts
+++ src/index.ts
@@ -19,9 +19,12 @@ export class FieldValidateError extends Error {
 
 interface UseFormConfig<Values extends Record<string, any>> {
     initialValues: Values;
-    validate: (values, meta: Meta<Values>) => Errors<Values> | Promise<Errors<Values>>;
+    validate: (values, meta: Meta<Values>, submit: boolean) => Errors<Values> | Promise<Errors<Values>>;
     onSubmit: (values: Values) => void | Promise<void>;
 }
+
+export type ValidateFunction<Values extends Record<string, any>> = UseFormConfig<Values>['validate'];
+export type OnSubmitFunction<Values extends Record<string, any>> = UseFormConfig<Values>['onSubmit'];
 
 function mapValues<Values extends Record<string, any>>(
     values: Values,
@@ -37,53 +40,166 @@ function mapValues<Values extends Record<string, any>>(
     return result;
 }
 
+type ErrorState = {
+    error?: boolean;
+    message?: string;
+    version: number;
+};
+
+type ErrorsState<Values extends Record<string, any>> = {
+    [K in keyof Values]: ErrorState;
+}
+
+let VERSION = 0;
+
+function mergeError<Values extends Record<string, any>>(states: ErrorsState<Values>, errors: Errors<Values>, meta: Meta<Values>, version: number): ErrorsState<Values> {
+    let change = false;
+    const newStates = {...states};
+    for (const key of Object.keys(states)) {
+        const newState = newStates[key];
+        if (version >= newState.version) {
+            if (key in errors) {
+                if ((errors[key] ?? undefined) === undefined) {
+                    newStates[key as keyof Values] = {
+                        error: false,
+                        message: undefined,
+                        version,
+                    };
+                    change = true;
+                } else {
+                    newStates[key as keyof Values] = {
+                        error: true,
+                        message: errors[key],
+                        version,
+                    };
+                    change = true;
+                }
+            } else if (meta[key].change) {
+                newStates[key as keyof Values] = {
+                    error: false,
+                    message: undefined,
+                    version,
+                };
+                change = true;
+            }
+        }
+    }
+    return change ? newStates : states;
+}
+
+function hasError<Values extends Record<string, any>>(
+  errors: ErrorsState<Values>,
+  errorsPatch: Errors<Values>, globalError: string | undefined | null,
+  meta: Meta<Values>, version: number,
+) {
+    const newErrors = mergeError(errors, errorsPatch, meta, version);
+    return Object.keys(newErrors).some(key => newErrors[key].error) || typeof globalError === 'string';
+}
+
 export const useForm = <Values extends Record<string, any>> ({ initialValues, validate, onSubmit }: UseFormConfig<Values>) => {
     const [values, setValues] = React.useState(initialValues);
-    const [errors, setErrors] = React.useState<Errors<Values>>({});
+    const [errors, setErrors] = React.useState<ErrorsState<Values>>(() => mapValues(initialValues, _ => {
+        const version = ++VERSION;
+        return {
+            error: undefined,
+            message: undefined,
+            version,
+        }
+    }));
+    const [validatingState, setValidatingState] = React.useState<number[]>([]);
+    const validating = validatingState.length > 0;
+    const [submitting, setSubmitting] = React.useState(false);
     const unmountRef = React.useRef<boolean>(false);
     React.useEffect(() => {
         return () => {
             unmountRef.current = true;
         }
     }, []);
-    const [globalError, setGlobalError] = React.useState<string | undefined>();
-    const setFieldError = React.useCallback((field: keyof Values, error?: string) => {
-        setErrors(preErrors => ({ ...preErrors, [field]: error }));
+    const [globalErrors, setGlobalErrors] = React.useState<ErrorState>(() => ({
+        error: undefined,
+        message: undefined,
+        version: ++VERSION,
+    }));
+    const setFieldError = React.useCallback((field: keyof Values, error?: string | null) => {
+        setErrors(preErrors => ({
+            ...preErrors,
+            [field]: {
+                error: error !== null && error !== undefined,
+                message: error ?? undefined,
+                version: ++VERSION,
+            },
+        }));
     }, []);
-    const execValidate = React.useCallback(async (values: Values, meta: Meta<Values>) => {
-        let error = false;
+    const setGlobalError = React.useCallback((error?: string | null) => {
+        setGlobalErrors({
+            error: error !== null && error !== undefined,
+            message: error ?? undefined,
+            version: ++VERSION,
+        });
+    }, []);
+
+    /**
+     * the return value represent whether or not has error.
+     * but it is only meaningful when using in submit.
+     * because only the validation in submit is exclusive mode.
+     */
+    const execValidate = React.useCallback(async (values: Values, meta: Meta<Values>, submit: boolean) => {
+        const version = ++VERSION;
+        let errorsPatch: Errors<Values> = {};
+        let globalErrorsPatch : string | undefined | null = undefined;
         try {
-            const errorsPatch = await validate(values, meta);
+            setValidatingState(prevState => ([...prevState, version]));
+            errorsPatch = await validate(values, meta, submit);
             if (!unmountRef.current) {
-                error = Object.keys(errorsPatch).length > 0;
-                setErrors(preErrors => ({
-                    ...preErrors,
-                    ...mapValues(meta, _ => undefined, (_, m) => m.change ),
-                    ...errorsPatch,
-                }));
+                setErrors(preErrors => {
+                    return mergeError(preErrors, errorsPatch, meta, version);
+                });
             }
         } catch (e) {
             if (!unmountRef.current) {
                 if (e instanceof FieldValidateError) {
-                    error = true;
-                    setErrors(preErrors => ({
-                        ...preErrors,
-                        ...mapValues(meta, _ => undefined, (_, m) => m.change ),
-                        [(e as FieldValidateError).field]: e.message,
-                    }));
-                } else if ('message' in e) {
-                    error = !!e.message;
-                    setGlobalError(e.message);
+                    errorsPatch = {
+                        [((e as FieldValidateError).field)]: `${e.message || ''}`,
+                    } as Errors<Values>;
+                    setErrors(preErrors => {
+                        return mergeError(preErrors, errorsPatch, meta, version);
+                    });
+                } else {
+                    globalErrorsPatch = typeof e === 'string' ? e : (e.message || '');
+                    const error: ErrorState = {
+                        error: true,
+                        message: typeof e === 'string' ? e : (e.message || ''),
+                        version,
+                    }
+                    setGlobalErrors(prevError => {
+                        if (version >= prevError.version) {
+                            return error;
+                        } else {
+                            return prevError;
+                        }
+                    });
                 }
             }
+        } finally {
+            setValidatingState(prevState => {
+                const index = prevState.indexOf(version);
+                if (index >= 0) {
+                    const newState = [...prevState];
+                    newState.splice(index, 1);
+                    return newState;
+                } else {
+                    return prevState;
+                }
+            });
         }
-        return error;
-    }, [validate]);
+        return [errorsPatch, globalErrorsPatch, version] as const;
+    }, [setValidatingState, validate, setErrors, setGlobalErrors]);
     const handleChanges = React.useMemo(() => mapValues(
         initialValues,
         key => {
             const meta = mapValues(initialValues, k => ({ change: k === key, blur: false }));
             return value => {
+                if (submitting) return;
                 let target = value;
                 if (value instanceof Event || value.originalEvent instanceof Event || value.nativeEvent instanceof Event) {
                     target = value.target.value;
@@ -91,38 +207,47 @@ export const useForm = <Values extends Record<string, any>> ({ initialValues, va
                 setValues(preValues => {
                     const newValues = { ...preValues, [key]: target };
                     // noinspection JSIgnoredPromiseFromCall
-                    execValidate(newValues, meta);
+                    execValidate(newValues, meta, false);
                     return newValues;
                 });
             }
         },
-    ), [execValidate]);
+    ), [submitting, execValidate]);
     const handleBlurs = React.useMemo(() => mapValues(
         initialValues,
         key => {
             const meta = mapValues(initialValues, k => ({ change: false, blur: k === key }));
             return () => {
+                if (submitting) return;
                 // noinspection JSIgnoredPromiseFromCall
-                execValidate(values, meta);
+                execValidate(values, meta, false);
             };
         },
-    ), [values, execValidate]);
-    const handleSubmit: React.FormEventHandler = React.useCallback(async (evt) => {
-        evt.preventDefault();
-        const meta = mapValues(initialValues, _ => ({ change: true, blur: false}));
-        const error = await execValidate(values, meta);
-        if (!error) {
-            await onSubmit(values);
+    ), [submitting, values, execValidate]);
+    const handleSubmit = React.useCallback(async (evt?: React.SyntheticEvent) => {
+        evt && evt.preventDefault && evt.preventDefault();
+        if (validating || submitting) return;
+        setSubmitting(true);
+        try {
+            const meta = mapValues(initialValues, _ => ({ change: false, blur: false }));
+            const [errorsPatch, newGlobalError, version] = await execValidate(values, meta, true);
+            if (!hasError(errors, errorsPatch, newGlobalError, meta, version) && !unmountRef.current) {
+                await onSubmit(values);
+            }
+        } finally {
+            setSubmitting(false);
         }
-    }, [execValidate, values]);
+    }, [validating, submitting, setSubmitting, execValidate, values, onSubmit]);
     return {
         values,
         errors,
-        globalError,
-        setGlobalError,
+        globalError: globalErrors,
         setFieldError,
+        setGlobalError,
         handleChanges,
         handleBlurs,
         handleSubmit,
+        submitting,
+        validating,
     }
 };
--- stories/index.stories.tsx
+++ stories/index.stories.tsx
@@ -1,93 +1,130 @@
 import * as React from 'react';
-import { Errors, FieldValidateError, useForm } from '../src';
+import { Errors, FieldValidateError, useForm, ValidateFunction } from '../src';
 
 export default { title: 'useForm' };
 
 const sleep = async (time: number) => new Promise(resolve => setTimeout(resolve, time));
 
+enum CheckingStatus {
+  Unchecked,
+  Checking,
+  Valid,
+  Invalid,
+}
+
+function printCheckingStatus(status: CheckingStatus) {
+  switch (status) {
+    case CheckingStatus.Checking:
+      return 'Checking...';
+    case CheckingStatus.Invalid:
+      return 'Invalid!';
+    case CheckingStatus.Valid:
+      return 'Valid';
+    case CheckingStatus.Unchecked:
+      return null;
+  }
+}
+
 export const Demo = () => {
-  const [checkingName, setCheckingName] = React.useState(false);
-  const [checkingEmail, setCheckingEmail] = React.useState(false);
-  const { errors, values, handleChanges, handleBlurs, handleSubmit } = useForm({
+  const [nameStatus, setNameStatus] = React.useState(CheckingStatus.Unchecked);
+  const [emailStatus, setEmailStatus] = React.useState(CheckingStatus.Unchecked);
+  const initialValue = {
+    userName: '',
+    email: '',
+    password: '',
+    conformedPassword: '',
+  };
+  const validate: ValidateFunction<typeof initialValue> = React.useCallback(async (values, meta, submit) => {
+    const errors: Errors<typeof values> = {};
+    if (meta.userName.change || meta.userName.blur || submit) {
+      if (meta.userName.change) {
+        setNameStatus(CheckingStatus.Unchecked);
+      }
+      if (!values.userName) {
+        throw new FieldValidateError('userName', '用户名是必须的');
+      }
+      if (nameStatus === CheckingStatus.Unchecked && (meta.userName.blur || submit)) {
+        setNameStatus(CheckingStatus.Checking);
+        await sleep(1000);
+        if (values.userName !== 'vipcxj') {
+          setNameStatus(CheckingStatus.Invalid);
+          throw new FieldValidateError('userName', '用户名已被使用');
+        }
+        setNameStatus(CheckingStatus.Valid);
+      }
+    }
+    if (meta.email.change || meta.email.blur || submit) {
+      if (meta.email.change) {
+        setEmailStatus(CheckingStatus.Unchecked);
+      }
+      if (!values.email) {
+        throw new FieldValidateError('email', '电子邮箱是必须的');
+      }
+      if (emailStatus === CheckingStatus.Unchecked && (meta.email.blur || submit)) {
+        setEmailStatus(CheckingStatus.Checking);
+        await sleep(3000);
+        if (values.email !== 'vipcxj@form.com') {
+          setEmailStatus(CheckingStatus.Invalid);
+          throw new FieldValidateError('email', '电子邮箱已被使用');
+        }
+        setEmailStatus(CheckingStatus.Valid);
+      }
+    }
+    if (meta.password.change || meta.password.blur || submit) {
+      if (!values.password) {
+        throw new FieldValidateError('password', '密码是必须的');
+      }
+    }
+    if (meta.conformedPassword.change || meta.password.blur || submit) {
+      if (!values.conformedPassword) {
+        throw new FieldValidateError('conformedPassword', '必须重复一遍密码');
+      }
+      if (values.conformedPassword !== values.password) {
+        throw new FieldValidateError('conformedPassword', '与第一次输入的密码不同');
+      }
+    }
+    return errors;
+  }, [nameStatus, emailStatus]);
+  const onSubmit = React.useCallback(async () => {
+    await sleep(2500);
+    alert('提交成功');
+  }, []);
+  const { errors, values, handleChanges, handleBlurs, handleSubmit, submitting, validating } = useForm({
     initialValues: {
       userName: '',
       email: '',
       password: '',
       conformedPassword: '',
     },
-    validate: async (values, meta) => {
-      const errors: Errors<typeof values> = {};
-      if (meta.userName.change || meta.userName.blur) {
-        if (!values.userName) {
-          throw new FieldValidateError('userName', '用户名是必须的');
-        }
-        if (meta.userName.blur) {
-          setCheckingName(true);
-          await sleep(1000);
-          setCheckingName(false);
-          if (values.userName !== 'vipcxj') {
-            throw new FieldValidateError('userName', '用户名已被使用');
-          }
-        }
-      }
-      if (meta.email.change || meta.email.blur) {
-        if (!values.email) {
-          throw new FieldValidateError('email', '电子邮箱是必须的');
-        }
-        if (meta.email.blur) {
-          setCheckingEmail(true);
-          await sleep(1000);
-          setCheckingEmail(false);
-          if (values.email !== 'vipcxj@form.com') {
-            throw new FieldValidateError('email', '电子邮箱已被使用');
-          }
-        }
-      }
-      if (meta.password.change) {
-        if (!values.password) {
-          throw new FieldValidateError('password', '密码是必须的');
-        }
-      }
-      if (meta.conformedPassword.change) {
-        if (!values.conformedPassword) {
-          throw new FieldValidateError('conformedPassword', '必须重复一遍密码');
-        }
-        if (values.conformedPassword !== values.password) {
-          throw new FieldValidateError('conformedPassword', '与第一次输入的密码不同');
-        }
-      }
-      return errors;
-    },
-    onSubmit: async () => {
-      await sleep(3000);
-      alert('提交成功');
-    },
+    validate,
+    onSubmit,
   });
   return (
       <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="userName">用户名</label>
           <input key="userName" value={values.userName} onChange={handleChanges.userName} onBlur={handleBlurs.userName} />
-          { checkingName ? 'Checking...' : '' }
-          { errors.userName }
+          { printCheckingStatus(nameStatus) }
+          { errors.userName.message || null }
         </div>
         <div>
           <label htmlFor="email">电子邮箱</label>
           <input key="email" value={values.email} onChange={evt => handleChanges.email(evt.target.value)} onBlur={handleBlurs.email} />
-          { checkingEmail ? 'Checking...' : '' }
-          { errors.email }
+          { printCheckingStatus(emailStatus) }
+          { errors.email.message || null }
         </div>
         <div>
           <label htmlFor="password">密码</label>
           <input key="password" type="password" value={values.password} onChange={evt => handleChanges.password(evt.target.value)} onBlur={handleBlurs.password} />
-          { errors.password }
+          { errors.password.message || null }
         </div>
         <div>
           <label htmlFor="conformedPassword">重复密码</label>
           <input key="conformedPassword" type="password" value={values.conformedPassword} onChange={evt => handleChanges.conformedPassword(evt.target.value)} onBlur={handleBlurs.conformedPassword} />
-          { errors.conformedPassword }
+          { errors.conformedPassword.message || null }
         </div>
         <button type="submit">Submit</button>
+        { submitting ? 'Submitting...' : validating ? 'Validating...' : null }
       </form>
   );
 };
